4c9c7bdd18578f11cba8deb912458310578a2d03
Eric Dennison <ericd@netdenizen.com>
2015-02-21 22:06:56 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,4 +1,9 @@
 import hello
 
+def printstuff():
+    for x in range(100):
+        print("{0} bottles of rootbeer on the wall!".format(x))
+
 if __name__ == "__main__": 
     print ("Woah, so NOT cool dude!")
+    printstuff()
