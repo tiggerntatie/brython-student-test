e3a4ed9a5a23314fd135dcf4aacfd6c9345c897c
Eric Dennison <ericd@netdenizen.com>
2015-03-04 09:25:23 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -5,5 +5,5 @@ def printstuff():
         print("{0} bottles of rootbeer on the wall!".format(x))
 
 if __name__ == "__main__": 
-    print ("Woah, so NOT cool dude!")
+    print ("Welcome to the __main__ section of the __main__.py file!")
     printstuff()
